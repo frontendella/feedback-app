8df28323a98fac48889bab4175218068181d46cb
Ella R <ryukhinaella@gmail.com>
2022-06-05 18:01:55 -0700

    feedback app complete

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,19 @@
+import {Link} from 'react-router-dom'
+import Card from "../components/shared/Card"
+
+
+function AboutPage() {
+  return <Card>
+     <div className="about">
+         <hi>About This Project</hi>
+         <p>Thi is a React app to leave feedback for a product or a service</p>
+         <p>Version: 1.0.0</p>
+
+         <p>
+             <Link to="/"> Back to Home</Link>
+         </p>
+     </div>
+     </Card>
+}
+
+export default AboutPage
